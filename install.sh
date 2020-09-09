20886c9b405777907c630041a218f01455b4c160
Hugo Moreau <hugo.moreau@epita.fr>
2020-09-09 18:39:51 +0200

    install.sh updated
    You can now choose between install or source your default configs.
    For sure, improvements have to come later.
    
     * install.sh : Here.

--- install.sh
+++ install.sh
@@ -3,4 +3,19 @@
 # (if not, refer to README)
 
 # So much efforts :
-cp -r UltiSnips/ ~/.vim/
+
+if [ $# -eq 1 ]
+then
+	if [ $1 == "install" ]
+	then
+		cp -r UltiSnips/ ~/.vim/
+		echo "Installation done."
+	fi
+	if [ $1 == "source" ]
+	then
+		cp -r ~/.vim/UltiSnips/ .
+		echo "Source done."
+	fi
+else
+	echo "Usage: ./install.sh source/insall"
+fi
